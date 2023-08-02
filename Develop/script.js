b7e9c50b26e1bdcafb286dfec789b41e09bcd206
caleb baca <calebabaca@gmail.com>
2023-08-01 18:59:36 -0700

    set up rows of schedule

--- Develop/script.js
+++ Develop/script.js
@@ -5,6 +5,29 @@
 var now = moment().format("H A")
 
 $("#currrentDay").text(today)
+
+//setting up hourly breakdown
+var planWorkday = [
+  {time: "9 AM",
+      event: ""},
+  {time: "10 AM",
+      event: ""},
+  {time: "11 AM",
+      event: ""},
+  {time: "12 PM",
+      event: ""},
+  {time: "1 PM",
+      event: ""},
+  {time: "2 PM",
+      event: ""},
+  {time: "3 PM",
+      event: ""},
+  {time: "4 PM",
+      event: ""},
+  {time: "5 PM",
+      event: ""}
+  
+];
  ````// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
